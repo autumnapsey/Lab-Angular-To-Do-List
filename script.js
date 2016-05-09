3cebeac2aac69bed1cf0b3eff3e3456cba895456
Autumn Apsey <autumnapsey@gmail.com>
2016-05-09 09:26:16 -0400

    debugged, list now works

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ var toDo = angular.module("toDo", []);
 
 toDo.controller("toDoController", function($scope){
 	$scope.toDoList = ["Add things to To Do list", "Figure out Angular"];
-});
 
-$scope.toDoThing = function(){
-	$scope.toDoList.push($scope.toDoEnter);
+
+$scope.toDoThing = function(toDoItem){
+	$scope.toDoList.push(toDoItem);
 	$scope.toDoEnter = "";
 };
+
+});
